266e097228942c3a062c84cb9b25946ac5da19cd
Renato Gaspar <renatomgaspar8@gmail.com>
2023-12-06 14:47:51 +0000

    Project Bases
    Done:
    - Connected with the openWeatherApi
    - Css Fixed (Responsive)
    
     Needs:
    - Fix when they search something that doesnt exists

--- src/App.js
+++ src/App.js
@@ -2,9 +2,61 @@ import React, {useState} from "react";
 import axios from "axios";
 
 function App() {
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=85f9be45df21ca7a5afe8fd8815f6bbc&units=metric`
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter'){
+      axios.get(apiUrl).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"
+        type="text"/>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}ºC</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name != undefined && 
+          <div className="bottom">
+          <div className="feels">
+            {data.main ? <p className="bold">{data.main.feels_like.toFixed()}ºC</p> : null}
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p className="bold">{data.main.humidity.toFixed()}%</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            {data.wind ? <p className="bold">{data.wind.speed} Km</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div>
+        }
+      </div>
     </div>
   );
 }
